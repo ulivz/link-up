4868ae6f93e5e22990b3b0c325fbc501c202a6bf
toxichl <472590061@qq.com>
2017-04-23 21:44:13 +0800

    add docs

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,19 @@
 module.exports = {
-	entry: "./src/main.js",
-	output: {
-		path: __dirname,
-		filename: "./dist/linkup.js",
-		library: 'linkup',
-		libraryTarget: 'umd'
-	},
-	module: {
-		loaders: [
-			{
-				test: /\.js$/,
-				exclude: /node_modules/,
-				loader: "babel-loader"
-			}
-		]
-	}
+    entry: "./src/main.js",
+    output: {
+        path: __dirname,
+        filename: "./dist/linkup.js",
+        library: 'linkup',
+        libraryTarget: 'umd'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            }
+        ]
+    }
 };
 
